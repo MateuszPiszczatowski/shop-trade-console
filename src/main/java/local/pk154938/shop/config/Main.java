0afe248a2605250216ea5f881b6cc9c783658d4e
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2026-03-24 15:03:40 +0100

    feat: init project structure and main menu

---
+++ src/main/java/local/pk154938/shop/config/Main.java
@@ -0,0 +1,9 @@
+package local.pk154938.shop.config;
+import local.pk154938.shop.ui.menu.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.show();
+    }
+}
---
+++ src/main/java/local/pk154938/shop/ui/menu/MainMenu.java
@@ -0,0 +1,7 @@
+package local.pk154938.shop.ui.menu;
+
+public class MainMenu {
+    public void show(){
+        System.out.println("Hello world");
+    }
+}
