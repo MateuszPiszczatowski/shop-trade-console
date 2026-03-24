9b434e5f8ca0e5a066ac4ff9eab066b9843db659
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2026-03-24 20:15:35 +0100

    feat: create basic User class

---
+++ src/main/java/local/pk154938/shop/domain/user/User.java
@@ -0,0 +1,19 @@
+package local.pk154938.shop.domain.user;
+
+public class User {
+    private final String username;
+    private final String hashedPassword;
+
+    public User(String username, String hashedPassword){
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+}
