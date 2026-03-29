cb8679131db121b83191fabcc512d092ff2f778e
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2026-03-29 11:10:56 +0200

    refactor: update switch for Java 11 syntax

--- src/main/java/local/pk154938/shop/ui/menu/MainMenu.java
+++ src/main/java/local/pk154938/shop/ui/menu/MainMenu.java
@@ -7,15 +7,15 @@ public class MainMenu {
     private final UserService userService;
     private final Session session;
 
-    public MainMenu(UserService userService, Session session){
+    public MainMenu(UserService userService, Session session) {
         this.userService = userService;
         this.session = session;
     }
 
-    public void show(){
+    public void show() {
         boolean running = true;
 
-        while(running){
+        while (running) {
             System.out.println("=== MENU GŁÓWNE ===");
             System.out.println("1. Zaloguj");
             System.out.println("2. Wyjście");
@@ -23,28 +23,34 @@ public class MainMenu {
 
             String choice = System.console().readLine();
 
-            switch (choice){
-                case "1" -> handleLogin();
-                case "2" -> running = false;
-                default -> System.out.println("Niepoprawny wybór.");
+            switch (choice) {
+                case "1":
+                    handleLogin();
+                    break;
+                case "2":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Niepoprawny wybór.");
             }
         }
         System.out.println("Zamykanie programu...");
     }
 
-    private void handleLogin(){
+    private void handleLogin() {
         System.out.print("Login: ");
         String login = System.console().readLine();
         System.out.print("Hasło: ");
         String password = System.console().readLine();
 
         var user = userService.login(login, password);
-        if(user == null){
+        if (user == null) {
             System.out.println("Błędne dane logowania");
-        }
-        else{
+        } else {
             session.login(user);
-            System.out.println("Zalogowano jako: "+user.getUsername());
+            System.out.println("Zalogowano jako: " + user.getUsername());
         }
-    };
+    }
+
+    ;
 }
